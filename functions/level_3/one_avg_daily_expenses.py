ceee96f72189e7ebddcf5e5da9b8bbb60d5b131a
Ilya Lebedev <melevir@gmail.com>
2023-12-12 09:06:50 +0100

    fix typo

--- functions/level_3/one_avg_daily_expenses.py
+++ functions/level_3/one_avg_daily_expenses.py
@@ -6,7 +6,7 @@ from functions.level_3.models import Expense
 
 
 def calculate_average_daily_expenses(expenses: list[Expense]) -> decimal.Decimal:
-    total_expenses_dy_day = collections.defaultdict(decimal.Decimal)
+    total_expenses_by_day = collections.defaultdict(decimal.Decimal)
     for expense in expenses:
-        total_expenses_dy_day[expense.spent_at.date()] += expense.amount
-    return mean(total_expenses_dy_day.values())
+        total_expenses_by_day[expense.spent_at.date()] += expense.amount
+    return mean(total_expenses_by_day.values())
